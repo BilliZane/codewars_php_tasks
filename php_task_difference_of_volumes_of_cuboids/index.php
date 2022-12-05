6aaa94bce41f2cd979a6e02db04406d6ccea1775
Vjacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2022-12-05 18:07:07 +0200

    The task «Difference of Volumes of Cuboids» was completed

---
+++ php_task_difference_of_volumes_of_cuboids/index.php
@@ -0,0 +1,12 @@
+<?php
+  function findDifference($a, $b) {
+    $arrProd1 = array_product($a);
+    $arrProd2 = array_product($b);
+
+    if($arrProd1 > $arrProd2) {
+      return $arrProd1 - $arrProd2;
+    }
+    return $arrProd2 - $arrProd1;
+  }
+
+echo findDifference([3, 2, 5], [1, 4, 4]);
